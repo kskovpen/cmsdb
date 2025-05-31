0c53fa6b2fdc12fa564ddda081408b7570b6b437
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2025-05-31 08:16:08 +0200

    update

--- cmsdb/campaigns/run3_2022_preEE_nano_exo_v12/ewk.py
+++ cmsdb/campaigns/run3_2022_preEE_nano_exo_v12/ewk.py
@@ -10,12 +10,12 @@ import cmsdb.processes as procs
 from cmsdb.campaigns.run3_2022_preEE_nano_exo_v12 import campaign_run3_2022_preEE_nano_exo_v12 as cpn
 
 cpn.add_dataset(
-    name="w_lnu_4jets_madgraph",
+    name="w_lnu_4_madgraph",
     id=14803995,
-    processes=[procs.w_lnu],
+    processes=[procs.w_lnu_4j],
     keys=[
         "/WtoLNu-4Jets_TuneCP5_13p6TeV_madgraphMLM-pythia8",  # noqa
     ],
-    n_files=1164,
+    n_files=1152,
     n_events=84739011,
 )
