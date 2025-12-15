022116151ae8b5faf574497c698b2f8f58217ce8
atomaru <adina.maria.tomaru@cern.ch>
2025-12-15 13:03:21 +0100

    heavyN, solved issues

--- cmsdb/processes/heavyn.py
+++ cmsdb/processes/heavyn.py
@@ -3,7 +3,6 @@ HeavyN process definitions.
 """
 
 __all__ = [
-
     "heavyn_m12_v10m6_mm", "heavyn_m14_v10m2_mm", "heavyn_m4_v10m2_mm", "heavyn_m4_v10m6_mm", "heavyn_m8_v10m4_mm",
 ]
 
@@ -14,8 +13,8 @@ from scinum import Number
 import cmsdb.constants as const
 # Naming: mN (2 difigits) | constant 10(power) | 11 for ee 22 for mm
 
-heavyn_m12_v10m6_mm = Process(name="heavyn_m12_v10m6_mm", id=1210622, label=r"$V=10^{-6}$ $m_N=12$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.491e-03)},)
-heavyn_m14_v10m2_mm = Process(name="heavyn_m14_v10m2_mm", id=1410222, label=r"$V=10^{-2}$ $m_N=14$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.491e-03)},)
-heavyn_m4_v10m2_mm = Process(name="heavyn_m4_v10m2_mm", id=410222, label=r"$V=10^{-2}$ $m_N=4$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.491e-03)},)
-heavyn_m4_v10m6_mm = Process(name="heavyn_m4_v10m6_mm", id=410622, label=r"$V=10^{-6}$ $m_N=4$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.491e-03)},)
-heavyn_m8_v10m4_mm = Process(name="heavyn_m8_v10m6_mm", id=810422, label=r"$V=10^{-6}$ $m_N=8$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.491e-03)},)
+heavyn_m12_v10m6_mm = Process(name="heavyn_m12_v10m6_mm", id=1210622, label=r"$V=10^{-6}$ $m_N=12$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.324e-09)},)
+heavyn_m14_v10m2_mm = Process(name="heavyn_m14_v10m2_mm", id=1410222, label=r"$V=10^{-2}$ $m_N=14$ GeV ($\mu \mu$)", xsecs={13.6: Number(0.2289)},)
+heavyn_m4_v10m2_mm = Process(name="heavyn_m4_v10m2_mm", id=410222, label=r"$V=10^{-2}$ $m_N=4$ GeV ($\mu \mu$)", xsecs={13.6: Number(0.2382)},)
+heavyn_m4_v10m6_mm = Process(name="heavyn_m4_v10m6_mm", id=410622, label=r"$V=10^{-6}$ $m_N=4$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.382e-09)},)
+heavyn_m8_v10m4_mm = Process(name="heavyn_m8_v10m4_mm", id=810422, label=r"$V=10^{-4}$ $m_N=8$ GeV ($\mu \mu$)", xsecs={13.6: Number(2.374e-05)},)
